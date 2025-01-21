221cf9eac556d562ef7f5e7103d1b73a9e75a461
vmangahis <vince.baron092299@gmail.com>
2025-01-21 12:29:18 +0800

    cleanup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,5 @@
-// import { useEffect, useState } from "react";
-// import sapphireuserApi from "../api/sapphireapi";
 import HeroSection from "../components/HeroSection";
 import QuestsHomePage from "../components/QuestsHomePage";
-
-// interface Monsters {
-//   monsterName: string;
-// }
 
 const Home = () => {
   return (
