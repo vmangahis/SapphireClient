29090a393622d934ebc2982a7ca2140c51fd04fd
vmangahis <vince.baron092299@gmail.com>
2025-01-25 12:46:01 +0800

    added icon for drawer mobile nav

--- src/components/DrawerNavMobile.tsx
+++ src/components/DrawerNavMobile.tsx
@@ -8,6 +8,9 @@ import {
   useTheme,
 } from "@mui/material";
 import useNavMenu from "../hooks/useNavMenu";
+import HomeIcon from "@mui/icons-material/Home";
+import PeopleIcon from "@mui/icons-material/People";
+import RequestPageIcon from "@mui/icons-material/RequestPage";
 
 const DrawerNavMobile = () => {
   const { toggleDrawer } = useNavMenu();
@@ -19,14 +22,32 @@ const DrawerNavMobile = () => {
       onClick={toggleDrawer(false)}
     >
       <List>
-        {["HOME", "HUNTERS", "QUESTS"].map((text) => (
-          <ListItem key={text}>
-            <ListItemButton>
-              <ListItemIcon></ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+        <ListItem key="HOME">
+          <ListItemButton>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="HOME" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem key="HUNTERS">
+          <ListItemButton>
+            <ListItemIcon>
+              <PeopleIcon />
+            </ListItemIcon>
+            <ListItemText primary="HUNTERS" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem key="QUESTS">
+          <ListItemButton>
+            <ListItemIcon>
+              <RequestPageIcon />
+            </ListItemIcon>
+            <ListItemText primary="QUESTS" />
+          </ListItemButton>
+        </ListItem>
       </List>
     </Box>
   );
